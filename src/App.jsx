010b1487dc961396558dc97af4b70126e65a15f3
Milos Rujevic <milosrujevic@gmail.com>
2022-08-21 21:39:39 +0200

    Add State and Reset Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useReducer } from 'react'
 import './App.css'
 import styled from 'styled-components'
 
@@ -7,54 +7,145 @@ import Input from './components/Input.jsx'
 
 import { QRCodeSVG } from 'qrcode.react'
 
+const initialState = {
+    payer: '',
+    paymentDescription: '',
+    receiver: '',
+    payCode: '',
+    currencyCode: 'RSD',
+    totalAmount: '',
+    accountReceivable: '',
+    modelCode: '',
+    paymentNumber: ''
+}
+
+const ACTIONS = {
+    PAYER_CHANGED: 'payer-change',
+    PAYMENT_DESCRIPTION: 'payment-description-change',
+    RECEIVER_CHANGED: 'receiver-change',
+    PAYCODE_CHANGED: 'pay-code-change',
+    CURRENCY_CHANGED: 'currency-change',
+    TOTAL_AMOUNT: 'total-amount-change',
+    ACCOUNT_RECEIVABLE: 'account-available-change',
+    MODEL_CODE: 'model-code-change',
+    PAYMENT_NUMBER: 'payment-number-change',
+    RESET_VALUES: 'reset-values'
+}
+
+const init = () => initialState;
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case ACTIONS.PAYER_CHANGED:
+            return {
+                ...state,
+                payer: action.payload
+            }
+        case ACTIONS.PAYMENT_DESCRIPTION:
+            return {
+                ...state,
+                paymentDescription: action.payload
+            }
+        case ACTIONS.RECEIVER_CHANGED:
+            return {
+                ...state,
+                receiver: action.payload
+            }
+        case ACTIONS.PAYCODE_CHANGED:
+            return {
+                ...state,
+                payCode: action.payload
+            }
+        case ACTIONS.CURRENCY_CHANGED:
+            return {
+                ...state,
+                currencyCode: action.payload
+            }
+        case ACTIONS.TOTAL_AMOUNT:
+            return {
+                ...state,
+                totalAmount: action.payload
+            }
+        case ACTIONS.ACCOUNT_RECEIVABLE:
+            return {
+                ...state,
+                accountReceivable: action.payload
+            }
+        case ACTIONS.MODEL_CODE:
+            return {
+                ...state,
+                modelCode: action.payload
+            }
+        case ACTIONS.PAYMENT_NUMBER:
+            return {
+                ...state,
+                paymentNumber: action.payload
+            }
+        case ACTIONS.RESET_VALUES:
+            return init()
+        default:
+            console.error('Action.type must be ' + action.type)
+            return state
+    }
+}
+
+
+
 function App() {
-    const [payer, setPayer] = useState('')
-    const onPayerChange = event => setPayer(event.target.value)
 
-    const [paymentDescription, setPaymentDescription] = useState('')
-    const onPaymentDescriptionChange = event => setPaymentDescription(event.target.value)
+    const [state, dispatch] = useReducer(reducer, initialState, init)
 
-    const [receiver, setReceiver] = useState('')
-    const onReceiverChange = event => setReceiver(event.target.value)
+    const resetValues = () => dispatch({ type: ACTIONS.RESET_VALUES })
 
-    const [payCode, setPayCode] = useState('')
-    const onPayCodeChange = event => setPayCode(event.target.value)
+    const onPayerChange = event => dispatch({ type: ACTIONS.PAYER_CHANGED, payload: event.target.value })
+    const onPaymentDescriptionChange = event =>
+        dispatch({ type: ACTIONS.PAYMENT_DESCRIPTION, payload: event.target.value })
+    const onReceiverChange = event => dispatch({ type: ACTIONS.RECEIVER_CHANGED, payload: event.target.value })
+    const onPayCodeChange = event => dispatch({ type: ACTIONS.PAYCODE_CHANGED, payload: event.target.value })
+    const onCurrencyCode = event => dispatch({ type: ACTIONS.CURRENCY_CHANGED, payload: event.target.value })
+    const onTotalAmountChange = event => dispatch({ type: ACTIONS.TOTAL_AMOUNT, payload: event.target.value })
+    const onAccountReceivableChange = event =>
+        dispatch({ type: ACTIONS.ACCOUNT_RECEIVABLE, payload: event.target.value })
 
-    const [currencyCode, setCurrencyCode] = useState('RSD')
-    const onCurrencyCode = event => setCurrencyCode(event.target.value)
+    const onSetModelCodeChange = event => dispatch({ type: ACTIONS.MODEL_CODE, payload: event.target.value })
+    const onPaymentNumberChange = event => dispatch({ type: ACTIONS.PAYMENT_NUMBER, payload: event.target.value })
 
-    const [totalAmount, setTotalAmount] = useState('')
-    const onTotalAmountChange = event => setTotalAmount(event.target.value)
-
-    const [accountReceivable, setAccountReceivable] = useState('')
-    const onAccountReceivableChange = event => setAccountReceivable(event.target.value)
-
-    const [modelCode, setModelCode] = useState('')
-    const onSetModelCodeChange = event => setModelCode(event.target.value)
-
-    const [paymentNumber, setPaymentChange] = useState('')
-    const onPaymentNumberChange = event => setPaymentChange(event.target.value)
-
-    const qrModel = `K:PR|V:01|C:1|R:${accountReceivable}|N:${receiver}|I:${currencyCode}${totalAmount}|P:${payer}|SF:${payCode}|S:${paymentDescription}|RO:${modelCode}${paymentNumber}`
+    const qrModel = `K:PR|V:01|C:1|R:${state.accountReceivable}|N:${state.receiver}|I:${state.currencyCode}${state.totalAmount}|P:${state.payer}|SF:${state.payCode}|S:${state.paymentDescription}|RO:${state.modelCode}${state.paymentNumber}`
 
     return (
         <Container>
             <BankSlipTitle>Nalog Za Uplatu</BankSlipTitle>
             <LeftSide>
-                <Textarea label='Platilac' value={payer} whenChanged={onPayerChange} />
-                <Textarea label='Svrha uplate' value={paymentDescription} whenChanged={onPaymentDescriptionChange} />
-                <Textarea label='Primalac' value={receiver} whenChanged={onReceiverChange} />
+                <Textarea label='Platilac' value={state.payer} whenChanged={onPayerChange} />
+                <Textarea
+                    label='Svrha uplate'
+                    value={state.paymentDescription}
+                    whenChanged={onPaymentDescriptionChange}
+                />
+                <Textarea label='Primalac' value={state.receiver} whenChanged={onReceiverChange} />
             </LeftSide>
             <RightSide>
-                <Input width={17} label='Sifra Pacanja' value={payCode} whenChanged={onPayCodeChange} />
-                <Input width={17} disabled={true} label='Valuta' value={currencyCode} whenChanged={onCurrencyCode} />
-                <Input width={66} label='Iznos' value={totalAmount} whenChanged={onTotalAmountChange} />
-                <Input label='Racun Primaoca' value={accountReceivable} whenChanged={onAccountReceivableChange} />
-                <Input width={25} label='Model' value={modelCode} whenChanged={onSetModelCodeChange} />
-                <Input width={75} label='Poziv na broj' value={paymentNumber} whenChanged={onPaymentNumberChange} />
+                <Input width={17} label='Sifra Pacanja' value={state.payCode} whenChanged={onPayCodeChange} />
+                <Input
+                    width={17}
+                    disabled={true}
+                    label='Valuta'
+                    value={state.currencyCode}
+                    whenChanged={onCurrencyCode}
+                />
+                <Input width={66} label='Iznos' value={state.totalAmount} whenChanged={onTotalAmountChange} />
+                <Input label='Racun Primaoca' value={state.accountReceivable} whenChanged={onAccountReceivableChange} />
+                <Input width={25} label='Model' value={state.modelCode} whenChanged={onSetModelCodeChange} />
+                <Input
+                    width={75}
+                    label='Poziv na broj'
+                    value={state.paymentNumber}
+                    whenChanged={onPaymentNumberChange}
+                />
 
                 <QRCodeSVG value={qrModel} />
             </RightSide>
+            <button onClick={resetValues}>Očisti vrednosti</button>
         </Container>
     )
 }
