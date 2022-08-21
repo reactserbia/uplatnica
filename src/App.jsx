69adfa22d7668b8dc9016d7a41492e0a2d081d38
Mirko Basic <bejzik8@gmail.com>
2022-08-21 23:59:59 +0200

    Added CSS reset.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import { useReducer } from 'react'
-import styled from 'styled-components'
+import styled, { createGlobalStyle } from 'styled-components'
+import { QRCodeSVG } from 'qrcode.react'
 
 import Textarea from './components/Textarea.jsx'
 import Input from './components/Input.jsx'
-
-import { QRCodeSVG } from 'qrcode.react'
 
 const initialState = {
     payer: '',
@@ -109,6 +108,7 @@ function App() {
 
     return (
         <Container>
+            <GlobalStyles />
             <BankSlipTitle>Nalog Za Uplatu</BankSlipTitle>
             <LeftSide>
                 <Textarea label='Platilac' value={state.payer} whenChanged={onPayerChange} />
@@ -145,17 +145,48 @@ function App() {
     )
 }
 
+const GlobalStyles = createGlobalStyle`
+    *, *::before, *::after {
+        box-sizing: border-box;
+        padding: 0;
+        margin: 0;
+    }
+    body, h1, h2, h3, h4, p, figure, blockquote, dl, dd { margin: 0; }
+    ul[role='list'], ol[role='list'] { list-style: none; }
+    html:focus-within { scroll-behavior: smooth; }
+    body {
+        min-height: 100vh;
+        text-rendering: optimizeSpeed;
+        line-height: 1.5;
+        overflow: hidden;
+    }
+    a:not([class]) { text-decoration-skip-ink: auto; }
+    img, picture {
+        max-width: 100%;
+    }
+    input, button, textarea, select { font: inherit; }
+    @media (prefers-reduced-motion: reduce) {
+        html:focus-within { scroll-behavior: auto; }
+        
+        *, *::before, *::after {
+        animation-duration: 0.01ms !important;
+        animation-iteration-count: 1 !important;
+        transition-duration: 0.01ms !important;
+        scroll-behavior: auto !important;
+        }
+    }
+`
+
 const Container = styled.div`
     width: 100%;
-    max-width: 771px;
+    max-width: 800px;
     position: relative;
-    border: solid 1px #fff;
+    line-height: 1.2em;
     font-family: Arial, Helvetica, sans-serif;
     font-size: 3mm;
-    line-height: 1.2em;
-    margin-left: auto;
-    margin-right: auto;
+    border: solid 1px #fff;
     padding: 6mm;
+
     &::before {
         border-right: solid 1px #fff;
         content: '';
