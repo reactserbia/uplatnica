9450c8897d1e24d004b352b526880919cfd8279c
Milos Rujevic <milosrujevic@gmail.com>
2022-08-21 21:56:12 +0200

    Add Animated FavIcon

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import Favicon from "react-favicon";
+import animatedIcon from "./icon.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
+        <Favicon animated={true} url={animatedIcon} />
         <App />
     </React.StrictMode>
 )
