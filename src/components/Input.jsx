d3357796832788ccc63dc014c36a187e287d58bf
Milos Rujevic <milosrujevic@gmail.com>
2022-08-21 20:18:12 +0200

    set currency code to RSD only

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,20 +1,19 @@
-import S from 'styled-components'
+import Label from "./Label.jsx";
+import S from "styled-components";
 
-import Label from './Label.jsx'
-
-const Input = ({ width, label, value, whenChanged }) => (
-    <Container width={width}>
-        <Label label={label} />
-        <InnerInput value={value} onChange={whenChanged} />
-    </Container>
-)
+const Input = ({ disabled, width, label, value, whenChanged }) => (
+  <Container width={width}>
+    <Label label={label} />
+    <InnerInput disabled={disabled} value={value} onChange={whenChanged} />
+  </Container>
+);
 
 const Container = S.div`
    display: inline-block;
    text-align: left;
    margin-bottom: 7px;
-   width: ${props => (props.width ? props.width : '100')}%;  
-`
+   width: ${(props) => (props.width ? props.width : "100")}%;  
+`;
 const InnerInput = S.input`
    width: calc(100% - 36px);
    background-color: lightgrey;
@@ -28,6 +27,9 @@ const InnerInput = S.input`
    appearance: none;
    outline: none;
    border-width: 0.7mm;
-`
+   &:disabled {
+    background: gray;
+   }
+`;
 
-export default Input
+export default Input;
