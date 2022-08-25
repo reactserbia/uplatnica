86cc1183f803835bf88f48e19d6878bfae9c8e1a
Basic Mirko <bejzik8@gmail.com>
2022-08-25 12:03:56 +0100

    Added absolute pathing configuration to Vite.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as path from 'path'
+
+const folders = ['', 'components', 'config', 'models', 'pages', 'utils']
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    resolve: {
+        alias: folders.map(folder => ({
+            find: `@${folder}`,
+            replacement: path.resolve(__dirname, `src/${folder}`)
+        }))
+    },
     plugins: [react()]
 })
