bbde5243a99b21f3175c915a77d73cd69dd642ed
Ivan Kovacevic <ivanbkovacevic@gmail.com>
2023-04-12 15:08:07 +0200

    feat/12-ux-add-bank-selectionn (#97)
    close #97

---
+++ src/constants/bankData.js
@@ -0,0 +1,90 @@
+const banksData = {
+    105: {
+        name: 'AGROINDUSTRIJSKO KOMERCIJALNA  BANKA AIK BANKA, AD, BEOGRAD',
+        img: '',
+    },
+    
+     115: {
+        name: 'MOBI BANKA, AD, BEOGRAD',
+        img: '',
+    },
+    145: {
+        name: 'EXPOBANK,AD,BEOGRAD',
+        img: '',
+    },
+    155: {
+        name: 'HALKBANK, AD, BEOGRAD',
+        img: '',
+    },
+    160: {
+        name: 'BANCAINTESA,AD,BEOGRAD',
+        img: '',
+    },
+    165: {
+        name: 'ADDIKO BANK, AD, BEOGRAD',
+        img: '',
+    },
+    170: {
+        name: 'UNICREDIT BANK SRBIJA, AD, BEOGRAD',
+        img: '',
+    },
+    190: {
+        name: 'ALTA BANKA, AD, BEOGRAD',
+        img: '',
+    },
+    200: {
+        name: 'BANKA POSTANSKA  STEDIONICA, AD, BEOGRAD',
+        img: '',
+    },
+    205: {
+        name: 'NLB KOMERCIJALNABANKA,AD, BEOGRAD',
+        img: '',
+    },
+    220: {
+        name: 'PROCREDIT BANK, AD, BEOGRAD',
+        img: '',
+    },
+    250: {
+        name: 'EUROBANK DIREKTNA, AD, BEOGRAD',
+        img: '',
+    },
+    265: {
+        name: 'RAIFFEISEN BANKA, AD, BEOGRAD',
+        img: '',
+    },
+    295: {
+        name: 'SRPSKA BANKA, AD, BEOGRAD',
+        img: '',
+    },
+    325: {
+        name: 'OTP BANKA SRBIJA AKCIONARSKO  DRUSTVO NOVI SAD',
+        img: '',
+    },
+    330: {
+        name: 'RBA BANKA, AD, NOVI SAD',
+        img: '',
+    },
+    340: {
+        name: 'ERSTE BANK, AD, NOVI SAD',
+        img: '',
+    },
+    370: {
+        name: '3 BANKA, AD, NOVI SAD',
+        img: '',
+    },
+    375: {
+        name: 'API BANK, AD, BEOGRAD',
+        img: '',
+    },
+    380: {
+        name: 'MIRABANK, AD, BEOGRAD',
+        img: '',
+    },
+    385: {
+        name: 'BANK OF CHINA SRBIJA, AD, BEOGRAD - NOVI BEOGRAD',
+        img: '',
+    },
+
+};
+
+export default banksData;
